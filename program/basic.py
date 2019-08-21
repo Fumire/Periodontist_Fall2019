cd61ed32b719ddec412fe828735161f3dd842d48
Jaewoong Lee <230@fumire.moe>
2019-08-21 11:36:49 +0900

    Create basic.py
    add get_data and get_tsne

---
+++ program/basic.py
@@ -0,0 +1,43 @@
+import os
+import pickle
+import pandas
+import scipy
+import sklearn
+import sklearn.manifold
+
+
+def get_data(file_name):
+    """
+    get data from file.
+    last modified: 2019-08-21T11:30:57+0900
+    """
+    data = pandas.read_csv("data/" + file_name, sep="\t", skiprows=1)
+
+    return data
+
+
+def get_tsne(file_name):
+    """
+    calculate tsne from file, and save data into pickle."
+    last modified
+    """
+    _pickle_file = "pickles/" + file_name + ".pkl"
+    if os.path.exists(_pickle_file):
+        with open(_pickle_file, "rb") as f:
+            return pickle.load(f)
+    else:
+        raw_data = get_data(file_name)
+        tsne = pandas.DataFrame(data=sklearn.manifold.TSNE(n_components=2, random_state=0).fit_transform(raw_data), columns=["TSNE-1", "TSNE-2"])
+        tsne["TSNE-1"] = scipy.stats.zscore(tsne["TSNE-1"])
+        tsne["TSNE-2"] = scipy.stats.zscore(tsne["TSNE-2"])
+        tsne["id"] = raw_data["#OTU ID"]
+
+        with open(_pickle_file, "wb") as f:
+            pickle.dump(tsne, f)
+
+        return tsne
+
+
+if __name__ == "__main__":
+    for filename in ["1.tsv", "2.tsv"]:
+        print(get_tsne(filename))
