4763cda5679319e3983a69aee657d8d148be6f93
Jaewoong Lee <230@fumire.moe>
2019-08-27 18:16:46 +0900

    Update classification.py
    add classification

--- program/classification.py
+++ program/classification.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import os
 import pickle
 import pandas
@@ -32,5 +33,63 @@ def get_feature_importances(file_name, level=6):
     return return_data
 
 
+def get_features(file_name, level=6):
+    """
+    get features in order of importances. Remove feature importances is zero.
+    last modified: 2019-08-27T17:13:38+0900
+    """
+    raw_features = get_feature_importances(file_name, level=level)
+    raw_features = list(filter(lambda x: x[0], raw_features))
+
+    return list(map(lambda x: x[1], raw_features))
+
+
+def change_number_to_feature(file_name, number, level=6):
+    """
+    change number to feature
+    last modified: 2019-08-27T17:12:07+0900
+    """
+    raw_features = get_features(file_name, level=level)
+
+    if number > 2 ** len(raw_features):
+        raise ValueError
+
+    check_list = list(map(lambda x: ((2 ** x) & number) != 0, range(2 ** len(raw_features))))
+
+    return list(filter(lambda x: check_list[raw_features.index(x)], raw_features))
+
+
+def run_test(function, file_name, level, processes=100):
+    """
+    execute test for given function.
+    last modified: 
+    """
+
+
+def classification_with_XGBClassifier(file_name, number, level, return_score=True):
+    """
+    classification with XGBClassifier
+    last modified: 2019-08-27T17:55:02+0900
+    """
+    raw_data = data.processed_data(file_name, level=level)
+
+    selected_features = change_number_to_feature(file_name, number, level)
+    raw_data = raw_data[selected_features + ["classification"]]
+
+    x_train = raw_data.drop(columns=["classification"])
+    y_train = raw_data["classification"]
+
+    model = xgboost.XGBClassifier(random_state=0)
+    model.fit(x_train, y_train, verbose=False)
+
+    y_test = model.predict(x_train)
+
+    if return_score:
+        return numpy.mean(list(map(lambda i: 1 if y_test[i] == y_train[i] else 0, range(len(y_test)))))
+    else:
+        return y_test
+
+
 if __name__ == "__main__":
-    print(get_feature_importances("1.tsv"))
+    print(classification_with_XGBClassifier("1.tsv", 1234, 1, True))
+    print(classification_with_XGBClassifier("1.tsv", 1234, 1, False))
