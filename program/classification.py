cc53b070d1752665d7665699ca2e53b1a32bbdc1
Jaewoong Lee <230@fumire.moe>
2019-08-29 13:47:25 +0900

    Update classification.py
    use k-fold algorithm

--- program/classification.py
+++ program/classification.py
@@ -7,12 +7,12 @@ import xgboost
 import data
 
 
-def get_feature_importances(file_name, level=6):
+def get_feature_importances(file_name, level=6, percentile=5):
     """
     get feature importances
-    last modified: 2019-08-26T23:34:51+0900
+    last modified: 2019-08-29T13:45:54+0900
     """
-    _pickle_file = "pickles/feature_importances_" + file_name + "_" + str(level) + ".pkl"
+    _pickle_file = "pickles/feature_importances_" + file_name + "_" + str(level) + "_" + str(percentile) + ".pkl"
     if os.path.exists(_pickle_file):
         with open(_pickle_file, "rb") as f:
             return pickle.load(f)
@@ -26,6 +26,7 @@ def get_feature_importances(file_name, level=6):
     model.fit(x_train, y_train)
 
     return_data = sorted(list(zip(model.feature_importances_, list(raw_data.columns))), reverse=True)
+    return_data = return_data[:len(return_data) * percentile // 100]
 
     with open(_pickle_file, "wb") as f:
         pickle.dump(return_data, f)
@@ -59,54 +60,63 @@ def change_number_to_feature(file_name, number, level=6):
     return list(filter(lambda x: check_list[raw_features.index(x)], raw_features))
 
 
-def run_test(function, file_name, level, processes=100):
+def run_test(function, file_name, level, processes=100, k_fold=5):
     """
     execute test for given function.
-    last modified: 2019-08-28T12:30:29+0900
+    last modified: 2019-08-29T13:26:22+0900
     """
     features = get_features(file_name, level)
     print(len(features))
 
     with multiprocessing.Pool(processes=processes) as pool:
-        score = pool.starmap(function, [(file_name, i, level, True) for i in range(1, 2 ** len(features))])
+        score = pool.starmap(function, [(file_name, i, level, True, k_fold) for i in range(1, 2 ** len(features))])
     best = int(numpy.argmax(score)) + 1
     combination = sorted(change_number_to_feature(file_name, best, level))
 
-    print(max(score), best, combination)
+    print(max(score), best, level, combination)
 
 
-def classification_with_XGBClassifier(file_name, number, level, return_score=True):
+def classification_with_XGBClassifier(file_name, number, level, return_score=True, k_fold=5):
     """
     classification with XGBClassifier
-    last modified: 2019-08-28T12:41:17+0900
+    last modified: 2019-08-29T13:40:37+0900
     """
-    _pickle_file = "pickles/classification_with_XGBClassifier_" + file_name + "_" + str(number) + "_" + str(level) + ".pkl"
+    _pickle_file = "pickles/classification_with_XGBClassifier_" + file_name + "_" + str(number) + "_" + str(level) + "_" + str(k_fold) + ".pkl"
     if os.path.exists(_pickle_file):
         with open(_pickle_file, "rb") as f:
-            y_train, y_test = pickle.load(f)
+            y_answer, y_predict = pickle.load(f)
     else:
-        raw_data = data.processed_data(file_name=file_name, level=level)
+        folded_data = data.processed_data(file_name=file_name, level=level, for_validation=True, k_fold=k_fold)
 
         selected_features = change_number_to_feature(file_name, number, level)
-        raw_data = raw_data[selected_features + ["classification"]]
+        for i, elem in enumerate(folded_data):
+            folded_data[i] = elem[selected_features + ["classification"]]
 
-        x_train = raw_data.drop(columns=["classification"])
-        y_train = raw_data["classification"]
+        y_answer, y_predict = list(), list()
+        for i in range(k_fold):
+            train_data = pandas.concat(list(map(lambda x: folded_data[i], list(filter(lambda x: x != i, range(k_fold))))), ignore_index=True)
+            test_data = folded_data[i]
 
-        model = xgboost.XGBClassifier(random_state=0)
-        model.fit(x_train, y_train, verbose=False)
+            x_train, y_train = train_data.drop(columns=["classification"]), train_data["classification"]
+            x_test, y_test = test_data.drop(columns=["classification"]), test_data["classification"]
 
-        y_test = model.predict(x_train)
+            model = xgboost.XGBClassifier(n_jobs=1, random_state=0)
+            model.fit(x_train, y_train, verbose=False, eval_set=[(x_test, y_test)])
+
+            y_answer.append(y_test)
+            y_predict.append(pandas.Series(model.predict(x_test)))
+
+        y_answer, y_predict = pandas.concat(y_answer, ignore_index=True), pandas.concat(y_predict, ignore_index=True)
 
         with open(_pickle_file, "wb") as f:
-            pickle.dump((y_train, y_test), f)
+            pickle.dump((y_answer, y_predict), f)
 
     if return_score:
-        return numpy.mean(list(map(lambda i: 1 if y_test[i] == y_train[i] else 0, range(len(y_test)))))
+        return numpy.mean(list(map(lambda x: 1 if x[0] == x[1] else 0, zip(y_answer, y_predict))))
     else:
-        return y_test
+        return y_predict
 
 
 if __name__ == "__main__":
-    run_test(classification_with_XGBClassifier, "1.tsv", 1)
-    run_test(classification_with_XGBClassifier, "2.tsv", 1)
+    run_test(classification_with_XGBClassifier, "1.tsv", 6)
+    run_test(classification_with_XGBClassifier, "2.tsv", 6)
