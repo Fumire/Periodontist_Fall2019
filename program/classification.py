bfec9f07aa89633ad487ed07e0d9ca32d9774537
Jaewoong Lee <230@fumire.moe>
2019-08-29 14:06:12 +0900

    Update classification.py
    add cutting method

--- program/classification.py
+++ program/classification.py
@@ -7,31 +7,39 @@ import xgboost
 import data
 
 
-def get_feature_importances(file_name, level=6, percentile=5):
+def get_feature_importances(file_name, level=6, percentile=None, top=10):
     """
     get feature importances
-    last modified: 2019-08-29T13:45:54+0900
+    last modified: 2019-08-29T14:06:01+0900
     """
-    _pickle_file = "pickles/feature_importances_" + file_name + "_" + str(level) + "_" + str(percentile) + ".pkl"
+    _pickle_file = "pickles/feature_importances_" + file_name + "_" + str(level) + ".pkl"
     if os.path.exists(_pickle_file):
         with open(_pickle_file, "rb") as f:
-            return pickle.load(f)
+            return_data = pickle.load(f)
+    else:
+        raw_data = data.processed_data(file_name, level=level)
 
-    raw_data = data.processed_data(file_name, level=level)
+        x_train = raw_data.drop(columns=["classification"])
+        y_train = raw_data["classification"]
 
-    x_train = raw_data.drop(columns=["classification"])
-    y_train = raw_data["classification"]
+        model = xgboost.XGBClassifier(n_jobs=-1, random_state=0)
+        model.fit(x_train, y_train)
 
-    model = xgboost.XGBClassifier(n_jobs=-1, random_state=0)
-    model.fit(x_train, y_train)
+        return_data = sorted(list(zip(model.feature_importances_, list(raw_data.columns))), reverse=True)
 
-    return_data = sorted(list(zip(model.feature_importances_, list(raw_data.columns))), reverse=True)
-    return_data = return_data[:len(return_data) * percentile // 100]
+        with open(_pickle_file, "wb") as f:
+            pickle.dump(return_data, f)
 
-    with open(_pickle_file, "wb") as f:
-        pickle.dump(return_data, f)
-
-    return return_data
+    if percentile is None and top is None:
+        return return_data
+    elif percentile is not None and top is not None:
+        raise ValueError
+    elif percentile is None and top is not None:
+        return return_data[:top]
+    elif percentile is not None and top is None:
+        return return_data[:len(return_data) * percentile // 100]
+    else:
+        raise ValueError
 
 
 def get_features(file_name, level=6):
