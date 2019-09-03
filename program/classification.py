3c2c6d57c65e53ace840eea23abea25afede92e6
Jaewoong Lee <230@fumire.moe>
2019-09-04 02:26:46 +0900

    Update classification.py
    add classification with random forest

--- program/classification.py
+++ program/classification.py
@@ -4,6 +4,7 @@ import pickle
 import pandas
 import numpy
 import sklearn
+import sklearn.ensemble
 import xgboost
 import data
 
@@ -170,7 +171,7 @@ def classification_with_SVC(file_name, number, level, return_score=True, k_fold=
             pickle.dump((y_answer, y_predict, y_index), f)
 
         if return_score:
-            return numpy.mean(list(map(lambda x: 1 if x[0] == x[1] else 0, zip(y_answer, y_predict, y_index))))
+            return numpy.mean(list(map(lambda x: 1 if x[0] == x[1] else 0, zip(y_answer, y_predict))))
         else:
             return y_answer, y_predict, y_index
 
@@ -220,7 +221,52 @@ def classification_with_KNeighbors(file_name, number, level, return_score=True, 
             return y_answer, y_predict, y_index
 
 
+def classification_with_RandomForest(file_name, number, level, return_score=True, k_fold=5, verbose=False):
+    """
+    classification with RandomForest
+    reference: https://scikit-learn.org/stable/modules/ensemble.html#forest
+    last modified: 2019-09-03T17:50:22+0900
+    """
+    _pickle_file = "pickles/classification_with_RandomForest_" + file_name + "_" + str(number) + "_" + str(level) + str(k_fold) + ".pkl"
+    if os.path.exists(_pickle_file):
+        with open(_pickle_file, "rb") as f:
+            y_answer, y_predict, y_index = pickle.load(f)
+
+        if return_score:
+            return numpy.mean(list(map(lambda x: 1 if x[0] == x[1] else 0, zip(y_answer, y_predict, y_index))))
+        else:
+            return y_answer, y_predict, y_index
+    else:
+        raw_data = data.processed_data(file_name=file_name, level=level)
+
+        x_data, y_data = raw_data[change_number_to_feature(file_name, number, level)], raw_data[["classification"]]
+
+        y_answer, y_predict, y_index = list(), list(), list()
+
+        kf = sklearn.model_selection.KFold(n_splits=k_fold, random_state=0)
+        for train_data_index, test_data_index in kf.split(X=x_data, y=y_data):
+            x_train, y_train = x_data.iloc[train_data_index, :], numpy.ravel(y_data.iloc[train_data_index, :].values.tolist(), order="C")
+            x_test, y_test = x_data.iloc[test_data_index, :], numpy.ravel(y_data.iloc[test_data_index, :].values.tolist(), order="C")
+
+            clf = sklearn.ensemble.RandomForestClassifier(random_state=0, n_estimators=100)
+            clf.fit(x_train, y_train)
+
+            y_answer.append(pandas.Series(y_test))
+            y_predict.append(pandas.Series(clf.predict(x_test)))
+            y_index += list(test_data_index)
+
+        y_answer, y_predict = pandas.concat(y_answer, ignore_index=False), pandas.concat(y_predict, ignore_index=False)
+
+        with open(_pickle_file, "wb") as f:
+            pickle.dump((y_answer, y_predict, y_index), f)
+
+        if return_score:
+            return numpy.mean(list(map(lambda x: 1 if x[0] == x[1] else 0, zip(y_answer, y_predict))))
+        else:
+            return y_answer, y_predict, y_index
+
+
 if __name__ == "__main__":
     for file_name in ["1.tsv", "2.tsv"]:
-        for function in [classification_with_XGBClassifier, classification_with_SVC, classification_with_KNeighbors]:
+        for function in [classification_with_XGBClassifier, classification_with_SVC, classification_with_KNeighbors, classification_with_RandomForest]:
             best, level = run_test(function, file_name, 5)
