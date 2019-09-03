6f8e13efa2c32bebb871e4b8eadbbbabc2b549a4
Jaewoong Lee <230@fumire.moe>
2019-09-03 17:40:08 +0900

    Update classification.py
    add K_Neighbors algorithm (https://scikit-learn.org/stable/auto_examples/neighbors/plot_classification.html#sphx-glr-auto-examples-neighbors-plot-classification-py)

--- program/classification.py
+++ program/classification.py
@@ -132,7 +132,7 @@ def classification_with_SVC(file_name, number, level, return_score=True, k_fold=
     """
     classification with SVC of SVM
     reference: https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html#sklearn.svm.SVC
-    last modified: 2019-08-29T17:19:26+0900
+    last modified: 2019-09-03T17:39:30+0900
     """
     _pickle_file = "pickles/classification_with_SVC_" + file_name + "_" + str(number) + "_" + str(level) + "_" + str(k_fold) + ".pkl"
     if os.path.exists(_pickle_file):
@@ -170,12 +170,57 @@ def classification_with_SVC(file_name, number, level, return_score=True, k_fold=
             pickle.dump((y_answer, y_predict, y_index), f)
 
         if return_score:
-            return clf.score(x_test, y_test)
+            return numpy.mean(list(map(lambda x: 1 if x[0] == x[1] else 0, zip(y_answer, y_predict, y_index))))
+        else:
+            return y_answer, y_predict, y_index
+
+
+def classification_with_KNeighbors(file_name, number, level, return_score=True, k_fold=5, verbose=False):
+    """
+    classification with Nearest Neighbors algorithm
+    reference: https://scikit-learn.org/stable/auto_examples/neighbors/plot_classification.html#sphx-glr-auto-examples-neighbors-plot-classification-py
+    last modified: 2019-09-03T17:38:27+0900
+    """
+    _pickle_file = "pickles/KNeighbors_" + file_name + "_" + str(number) + "_" + str(level) + "_" + str(k_fold) + ".pkl"
+    if os.path.exists(_pickle_file):
+        with open(_pickle_file, "rb") as f:
+            y_answer, y_predict, y_index = pickle.load(f)
+
+        if return_score:
+            return numpy.mean(list(map(lambda x: 1 if x[0] == x[1] else 0, zip(y_answer, y_predict, y_index))))
+        else:
+            return y_answer, y_predict, y_index
+    else:
+        raw_data = data.processed_data(file_name=file_name, level=level)
+
+        selected_features = change_number_to_feature(file_name, number, level)
+        x_data, y_data = raw_data[selected_features], raw_data[["classification"]]
+
+        y_answer, y_predict, y_index = list(), list(), list()
+        kf = sklearn.model_selection.KFold(n_splits=k_fold, random_state=0)
+        for train_data_index, test_data_index in kf.split(X=x_data, y=y_data):
+            x_train, y_train = x_data.iloc[train_data_index, :], numpy.ravel(y_data.iloc[train_data_index, :].values.tolist(), order="C")
+            x_test, y_test = x_data.iloc[test_data_index, :], numpy.ravel(y_data.iloc[test_data_index, :].values.tolist(), order="C")
+
+            clf = sklearn.neighbors.KNeighborsClassifier()
+            clf.fit(x_train, y_train)
+
+            y_answer.append(pandas.Series(y_test))
+            y_predict.append(pandas.Series(clf.predict(x_test)))
+            y_index += list(test_data_index)
+
+        y_answer, y_predict = pandas.concat(y_answer, ignore_index=False), pandas.concat(y_predict, ignore_index=False)
+
+        with open(_pickle_file, "wb") as f:
+            pickle.dump((y_answer, y_predict, y_index), f)
+
+        if return_score:
+            return numpy.mean(list(map(lambda x: 1 if x[0] == x[1] else 0, zip(y_answer, y_predict, y_index))))
         else:
             return y_answer, y_predict, y_index
 
 
 if __name__ == "__main__":
     for file_name in ["1.tsv", "2.tsv"]:
-        for function in [classification_with_XGBClassifier, classification_with_SVC]:
+        for function in [classification_with_XGBClassifier, classification_with_SVC, classification_with_KNeighbors]:
             best, level = run_test(function, file_name, 5)
