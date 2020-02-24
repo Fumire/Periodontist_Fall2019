9edd3b9803765bdff04cd1c3941137857029c7aa
Jaewoong Lee <230@fumire.moe>
2020-02-24 14:34:47 +0900

    Add some methods to convert value

--- program/general.py
+++ program/general.py
@@ -23,5 +23,34 @@ def check_exist(file_name):
     return file_name
 
 
+def class_to_num(given_classes):
+    for something in given_classes:
+        if something not in classes:
+            raise KeyError(something)
+    return sum(list(map(lambda x: 2 ** x[0], list(filter(lambda x: x[1] in given_classes, list(enumerate(classes)))))))
+
+
+def num_to_class(num):
+    if num > 2 ** len(classes) or num < 1:
+        raise ValueError
+    return list(map(lambda x: x[1], list(filter(lambda x: 2 ** x[0] & num, list(enumerate(classes))))))
+
+
+def bacteria_to_num(bacteria):
+    for something in bacteria:
+        if something not in whole_values:
+            raise KeyError(something)
+    return sum(list(map(lambda x: 2 ** x[0], list(filter(lambda x: x[1] in bacteria, list(enumerate(whole_values)))))))
+
+
+def num_to_bacteria(num):
+    if num > 2 ** len(whole_values) or num < 1:
+        raise ValueError
+    return list(map(lambda x: x[1], list(filter(lambda x: 2 ** x[0] & num, list(enumerate(whole_values))))))
+
+
 if __name__ == "__main__":
-    pass
+    print(class_to_num(classes[:-2]))
+    print(num_to_class(23))
+    print(bacteria_to_num(whole_values))
+    print(num_to_bacteria(12345))
