fb1f3876e4aa407ec0604869a7f8e48172da41cb
Jaewoong Lee <230@fumire.moe>
2020-02-24 16:55:48 +0900

    Add statistical method

--- program/general.py
+++ program/general.py
@@ -4,7 +4,7 @@ import os
 
 absolute_values = sorted(["Aa", "Pg", "Tf", "Td", "Pi", "Fn", "Pa", "Cr", "Ec"])
 relative_values = sorted(["Aa_relative", "Pg_relative", "Tf_relative", "Td_relative", "Pi_relative", "Fn_relative", "Pa_relative", "Cr_relative", "Ec_relative"])
-whole_values = sorted(absolute_values + relative_values)
+whole_values = absolute_values + relative_values
 
 classes = ["Healthy", "Slight", "Moderate", "Severe", "Acute"]
 two_class_combinations = itertools.combinations(classes, 2)
@@ -48,6 +48,14 @@ def num_to_bacteria(num):
     if num > 2 ** len(whole_values) or num < 0:
         raise ValueError
     return list(map(lambda x: x[1], list(filter(lambda x: 2 ** x[0] & num, list(enumerate(whole_values))))))
+
+
+def aggregate_confusion_matrix(confusion_matrix=None):
+    if confusion_matrix is None:
+        return ("sensitivity", "specificity", "precision", "negative_predictive_value", "miss_rate", "fall_out", "false_discovery_rate", "false_ommission_rate", "thread_score", "accuracy", "F1_score", "odds_ratio")
+
+    TP, FP, FN, TN = confusion_matrix[0][0], confusion_matrix[0][1], confusion_matrix[1][0], confusion_matrix[1][1]
+    return (TP / (TP + FN), TN / (TN + FP), TP / (TP + FP), TN / (TN + FN), FN / (FN + TP), FP / (FP + TN), FP / (FP + TP), FN / (FN + TN), TP / (TP + FN + FP), (TP + TN) / (TP + TN + FP + FN), 2 * TP / (2 * TP + FP + FN), (TP / FP) / (FN / TN))
 
 
 if __name__ == "__main__":
