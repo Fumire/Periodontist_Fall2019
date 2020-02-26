ae41162ceba8fd1655f59a4d4a5dbdd23721b636
Jaewoong Lee <230@fumire.moe>
2020-02-26 18:41:52 +0900

    Add min/max value plot

--- program/draw_classification.py
+++ program/draw_classification.py
@@ -20,12 +20,42 @@ def draw_statistics(csv_file, output_dir):
         for statistics_value in sorted(general.aggregate_confusion_matrix(None)):
             seaborn.set(context="poster", style="whitegrid")
             fig, ax = matplotlib.pyplot.subplots(figsize=(24, 24))
-
-            seaborn.lineplot(x="feature_num", y=statistics_value, hue="classifier", ax=ax, legend="full", data=selected_data, hue_order=sorted(set(statistics_data["classifier"])))
-
+            seaborn.lineplot(x="feature_num", y=statistics_value, hue="classifier", ax=ax, legend="full", data=selected_data, hue_order=sorted(set(statistics_data["classifier"])), estimator="median", ci="sd")
             ax.set_title(combined_class.replace("-", " "))
+            fig.savefig(general.check_exist(os.path.join(output_dir, "Median_" + combined_class + "_" + statistics_value + ".png")))
+            matplotlib.pyplot.close(fig)
 
-            fig.savefig(general.check_exist(os.path.join(output_dir, combined_class + "_" + statistics_value + ".png")))
+            seaborn.set(context="poster", style="whitegrid")
+            fig, ax = matplotlib.pyplot.subplots(figsize=(24, 24))
+            seaborn.lineplot(x="feature_num", y=statistics_value, hue="classifier", ax=ax, legend="full", data=selected_data, hue_order=sorted(set(statistics_data["classifier"])))
+            ax.set_title(combined_class.replace("-", " "))
+            fig.savefig(general.check_exist(os.path.join(output_dir, "Mean_" + combined_class + "_" + statistics_value + ".png")))
+            matplotlib.pyplot.close(fig)
+
+
+def draw_extreme(csv_file, output_dir):
+    if not os.path.isfile(csv_file):
+        raise ValueError(csv_file)
+
+    statistics_data = pandas.read_csv(csv_file)
+    statistics_data["feature_num"] = list(map(lambda x: len(general.num_to_bacteria(x)), statistics_data["Number"]))
+
+    for combined_class in sorted(set(statistics_data["combined_class"])):
+        selected_data = statistics_data.loc[(statistics_data["combined_class"] == combined_class)]
+
+        for statistics_value in sorted(general.aggregate_confusion_matrix(None)):
+            seaborn.set(context="poster", style="whitegrid")
+            fig, ax = matplotlib.pyplot.subplots(figsize=(24, 24))
+            seaborn.lineplot(x="feature_num", y=statistics_value, hue="classifier", ax=ax, legend="full", data=selected_data, hue_order=sorted(set(statistics_data["classifier"])), estimator=min, ci=None)
+            ax.set_title(combined_class.replace("-", " "))
+            fig.savefig(general.check_exist(os.path.join(output_dir, "Min_" + combined_class + "_" + statistics_value + ".png")))
+            matplotlib.pyplot.close(fig)
+
+            seaborn.set(context="poster", style="whitegrid")
+            fig, ax = matplotlib.pyplot.subplots(figsize=(24, 24))
+            seaborn.lineplot(x="feature_num", y=statistics_value, hue="classifier", ax=ax, legend="full", data=selected_data, hue_order=sorted(set(statistics_data["classifier"])), estimator=max, ci=None)
+            ax.set_title(combined_class.replace("-", " "))
+            fig.savefig(general.check_exist(os.path.join(output_dir, "Max_" + combined_class + "_" + statistics_value + ".png")))
             matplotlib.pyplot.close(fig)
 
 
@@ -38,10 +68,13 @@ if __name__ == "__main__":
 
     group1 = parser.add_mutually_exclusive_group(required=True)
     group1.add_argument("--stat", help="Draw statistics values", action="store_true", default=False)
+    group1.add_argument("--extreme", help="Draw extreme values", action="store_true", default=False)
 
     args = parser.parse_args()
 
     if args.stat:
         draw_statistics(args.input_file, args.output_dir)
+    elif args.extreme:
+        draw_extreme(args.input_file, args.output_dir)
     else:
         exit("Something went wrong")
